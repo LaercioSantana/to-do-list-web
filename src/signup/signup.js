032379f8a542ce62fd28c3fbbb5c4d32e93f2251
Laercio Santana <laerciosantana021@hotmail.com>
2017-04-23 15:18:27 -0300

    implemented user creation

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 
+import Config from '../Config.js';
 import '../style/signup.scss';
 
 let signupComponent = () => {
@@ -10,6 +11,16 @@ let signupComponent = () => {
   }
 };
 
+var HTTP_STATUS = {
+  OK: 200,
+  CREATED: 201,
+  CONFLICT: 409,
+  BAD_REQUEST: 400,
+  INTERNAL_ERROR: 500,
+}
+
+var successLoginUrl = '/todos';
+
 class SignupCtrl {
   constructor($scope, $cookies, $http, $location) {
     this.$scope = $scope;
@@ -17,7 +28,36 @@ class SignupCtrl {
     this.$http = $http;
     this.$location = $location;
 
+    this.$scope.register = this.register.bind(this);
 
+    this.$scope.createStatus = undefined;
+    this.$scope.STATUS = HTTP_STATUS;
+  }
+
+  register(){
+    if(!this.$scope.signupForm.$invalid){
+      this.$http({
+        method: 'POST',
+        url: Config.api.rootUrl + '/users',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          full_name: this.$scope.name,
+          email: this.$scope.email,
+          password: this.$scope.password
+        }
+      }).then(function successCallback(response) {
+        if(Config.debug) console.log("create success");
+          this.$cookies.put(Config.cookies.TOKEN, response.data.token);
+          this.$scope.createStatus = 200;
+          this.$location.url(successLoginUrl);
+        }.bind(this), function errorCallback(response) {
+          if(Config.debug) console.log("create error");
+          if(Config.debug) console.log(response)
+          this.$scope.createStatus = response.status;
+        }.bind(this));
+    }
   }
 }
 
